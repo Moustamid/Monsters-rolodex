e692d7f1bfd6e00ecfcea59e4d143a176c702951
Karim <55461899+Moustamid@users.noreply.github.com>
2020-08-20 14:53:55 +0100

    adding files for githup pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {Component} from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component.jsx';
+import { SearchBox } from './components/search-box/search-box.component.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+   
+   constructor() {
+
+     super(); // enable us to use the Component constructor
+
+     this.state = {
+
+      monsters : [] , 
+      searchField : ''
+
+     };
+
+     
+
+   }
+     
+     // This is a Life Cycle Methode : integrated into the Component ' React.ComponentLifecycle ' .
+   // ++ they are called at diff. stagece when this componemt get rendred .
+ //   ++ Mount : is when React put the Component on the DOM for the first time : 
+      /*  it will call  componentDidMount by defaulr  */
+   componentDidMount() { 
+      // we fetch 'make an API request'
+       fetch('https://jsonplaceholder.typicode.com/users')
+       .then( Response => Response.json() )
+       .then( users =>  this.setState({ monsters: users }) );
+      
+   } 
+
+   handleChange = e => {
+        
+      this.setState(  { searchField : e.target.value }  ) ;
+   };
+
+
+
+
+   render() {   
+
+      // const monsters = this.state.monsters; 
+      // const searchField = this.state.searchField; 
+      const { monsters , searchField  } = this.state;
+      const filteredMonsters = monsters.filter( monster => (
+        monster.name.toLowerCase().includes( searchField.toLocaleLowerCase() ) 
+      ) );
+
+
+      return ( 
+
+          <div className="App">
+             <h1>Monsters Rolodex</h1>
+             <SearchBox 
+             placeholder= {'Search Monsters'} 
+             handleChange=  { this.handleChange }    
+             />
+             <CardList monsters ={filteredMonsters} />
+          </div>  
+
+      );
+
+   }
 }
 
 export default App;
+  
