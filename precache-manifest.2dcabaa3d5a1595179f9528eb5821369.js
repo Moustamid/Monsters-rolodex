df4a005b4df7bc5e226fd67bc81b5cc69ac357e4
MOUSTAMID <karim1992moustamid@gmail.com>
2020-08-21 21:04:59 +0100

    Updates

---
+++ precache-manifest.2dcabaa3d5a1595179f9528eb5821369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2bd0d2b5dc91def0f222e9523f8044",
+    "url": "/Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e2d4501bfcdc03805176",
+    "url": "/Monsters-rolodex/static/css/main.cfcf52a2.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2d4501bfcdc03805176",
+    "url": "/Monsters-rolodex/static/js/main.3749528e.chunk.js"
+  },
+  {
+    "revision": "ce236435b1a77080e948",
+    "url": "/Monsters-rolodex/static/js/runtime-main.9e6da6d0.js"
+  }
+]);
